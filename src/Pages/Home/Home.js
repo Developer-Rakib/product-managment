17fa3c6ef397f65dc732ada7a002c324db31983f
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-04-23 17:32:26 +0600

    dislay data from database

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,34 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Product from '../Product/Product';
 
 const Home = () => {
-    const products = [
-        {
-            id: 1,
-            name: 'Basic Tee',
-            imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-            price: '$35',
-            color: 'Black',
-
-        },
-        {
-            id: 1,
-            name: 'Basic Tee',
-            imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-            price: '$35',
-            color: 'Black',
-
-        },
-        {
-            id: 1,
-            name: 'Basic Tee',
-            imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-            price: '$35',
-            color: 'Black',
-
-        },
-        // More products...
-    ]
+    let [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/products")
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+    console.log(products);
     return (
         <div className="bg-white">
             <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
